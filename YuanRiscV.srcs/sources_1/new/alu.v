5dc13da2b5c500f2f4633c434d715ef78ad848a7
Yuan Jin <jin_yuan0928@163.com>
2023-05-24 22:10:43 +0200

    first commit

---
+++ YuanRiscV.srcs/sources_1/new/alu.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/24/2023 10:10:20 PM
+// Design Name: 
+// Module Name: alu
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module alu(
+
+    );
+endmodule
